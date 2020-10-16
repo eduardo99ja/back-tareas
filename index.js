b5fb467179922e7f0d4a69f61414f2d938dfb838
Eduardo Apodaca <eduardo1ja99@gmail.com>
2020-10-16 14:45:52 -0500

    solucionando ..

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ app.use(cors());
 app.use(express.json({ extended: true }));
 
 //puerto de la app
-const port = process.env.port || 4000;
+const PORT = process.env.PORT || 4000;
 
-//importar rutas
+//imPORTar rutas
 
 app.use("/api/usuarios", require("./routes/usuarios"));
 app.use("/api/auth", require("./routes/auth"));
@@ -27,6 +27,6 @@ app.use("/api/tareas", require("./routes/tareas"));
 
 //arrancar la app
 
-app.listen(port,'0.0.0.0', () => {
-  console.log(`Habilitado el puerto: ${port}`);
+app.listen(PORT, () => {
+  console.log(`Habilitado el puerto: ${PORT}`);
 });
